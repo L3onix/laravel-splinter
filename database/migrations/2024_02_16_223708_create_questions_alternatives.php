be20661dc85df1eab804ef8ce63797264349205e
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-02-16 19:46:15 -0300

    primeiro commit

---
+++ database/migrations/2024_02_16_223708_create_questions_alternatives.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions_alternatives', function (Blueprint $table) {
+            $table->id();
+            $table->text('alternative_text');
+            $table->boolean('is_correct');
+            $table->foreignId('questions_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions_alternatives');
+    }
+};
